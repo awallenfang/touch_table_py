a411dfcceec0512f0d293a0a591dbe98ca977a7d
Tinkerpad-Software <philippkruppe@web.de>
2022-09-16 21:29:02 +0200

    Cleaned Code up

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from machine import Pin, PWM
 from ir_tx.nec import NEC
 import math
 
-# digits to 7 segment display
+# Digits to 7 segment display
 lookup = [
     [1,1,1,1,1,1,0], # 0
     [0,1,1,0,0,0,0], # 1
@@ -20,21 +20,17 @@ lookup = [
 
 minutes_left = 0
 
-# CRUDE TIMER VARIANT
-#start = time.ticks_ms() # get millisecond counter
-#delta = time.ticks_diff(time.ticks_ms(), start) # compute time difference
-
+# Initialise Piezo
 piezo = machine.Pin(22)
 piezo_pin = machine.PWM(piezo)
 
-
-
-# ORIGNIAL TIMER
-#minute_timer = Timer(period=60000, mode=Timer.PERIODIC, callback=lambda t: minutes_left -= 1)  # type: ignore
-#minute_timer.init(period=60000, mode=Timer.PERIODIC, callback=lambda t: minutes_left -= 1)  # type: ignore
-
+# Initialise Button
 button_pin = Pin(21, Pin.IN, Pin.PULL_UP)
 
+# Initialise NEC Transmitter
+nec = NEC(Pin(16, Pin.OUT)) 
+
+# Initialise 7 Segment Displays
 tens_a = Pin(13, Pin.OUT)
 tens_b = Pin(12, Pin.OUT)
 tens_c = Pin(2, Pin.OUT)
@@ -57,17 +53,12 @@ tens_digit = 0
 ones_digit = 0
 
 
-nec = NEC(Pin(16, Pin.OUT)) # Add NEC Transmitter
 
-delta = 0
-start_time = time.ticks_ms()
-print("started")
+# Initialise Timer
+delta = 0 # Delta between Now and Timer starttime
+start_time = time.ticks_ms() # Moment Button was pressed
 
-
-#piezo_pin.freq(500)
-#piezo_pin.duty_u16(512)
-
-
+# Piezo Helper Subroutines
 def piezo_sound_turn_on(pin):
     pin.freq(420) # frequency in Hz [Range 10Hz to 12000Hz]
     pin.duty_u16(512) # Dutycyle (Volume) [Range 0 (Silent/Off) to 1000 (Full blast)]
@@ -78,22 +69,25 @@ def piezo_sound_turn_on(pin):
     time.sleep(0.4)
     pin.duty_u16(0)
     
-
 def piezo_sound_turn_off(piezo_pin):
     return
 
 def piezo_sound_remider(piezo_pin):
     return
+    
+def piezo_sound_button_press(piezo_pin):
+    return
 
+#Loop-de-Loop
 while True:
+    
     # If button is pressed, reset minutes left and start new timers
     if button_pin.value() == 0:
         if delta == 0: # If beamer was off, it will turn on.
-            nec.transmit(0xCA8B, 0x12) # turn beamer on
-            time.sleep(1)
-            print("Turnon")
             piezo_sound_turn_on(piezo_pin)
-        
+            time.sleep(0.25)
+            nec.transmit(0xCA8B, 0x12) # turn beamer on
+            time.sleep(0.25)
         start_time = time.ticks_ms() # get millisecond counter
         delta = 1
         
@@ -104,12 +98,12 @@ while True:
         minutes_left = 15 - math.floor(delta/10/60)
             
     # drunk coding lmao yeet
-    #delta = time.ticks_diff(time.ticks_ms(), start_time)
-    if delta >= 9000: #900.000 ms = 15mins
+    # delta = time.ticks_diff(time.ticks_ms(), start_time)
+    if delta >= 900000: #900.000 ms = 15mins
         delta = 0 # gotta make sure, computers are fucky wucky sometimes uwu
         nec.transmit(0xCA8B, 0x12)  # address == 0xCA8B, data == 0x12
         time.sleep(3)
-        nec.transmit(0xCA8B, 0x12) # Shutting the beamer off requires two (2) button presses
+        nec.transmit(0xCA8B, 0x12) # Shutting the beamer off requires two (2) consecutive button presses
         time.sleep(1)
         minutes_left = 0
         piezo_sound_turn_off(piezo_pin)
@@ -139,4 +133,3 @@ while True:
     ones_e.value(lookup[ones_digit][4])  # type: ignore
     ones_f.value(lookup[ones_digit][5])  # type: ignore
     ones_g.value(lookup[ones_digit][6])  # type: ignore
-
